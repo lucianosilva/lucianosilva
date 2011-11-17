caedd26ce0c9ae8411f341c22482ac92b3941a0b
luciano@lucianosilva.com <luciano@lucianosilva.com@878331a1-c259-cb4f-48cd-a76829f63afa>
2011-11-17 02:23:15 +0000

    atualização da estrutura do projeto.

---
+++ trunk/blog/Laboratorio-2/laboratorio2-core/src/main/java/com/lucianosilva/lab/core/repository/ClubeRepository.java
@@ -0,0 +1,29 @@
+/**
+ *  Licença Creative Commons.
+ *  Blog do Luciano Silva - www.lucianosilva.com
+ */
+package com.lucianosilva.lab.core.repository;
+
+import java.util.List;
+
+import org.codehaus.grepo.query.commons.annotation.GenericQuery;
+import org.codehaus.grepo.query.jpa.repository.ReadWriteJpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucianosilva.lab.core.entity.Clube;
+
+/**
+ * 
+ *
+ * @autor luciano.silva
+ * @version $Rev$ $Date$
+ */
+@Repository
+public interface ClubeRepository extends ReadWriteJpaRepository<Clube, Long> {
+
+
+	@GenericQuery( query = "from Clube where 1=1" )
+	List<Clube> findAll();
+
+
+}

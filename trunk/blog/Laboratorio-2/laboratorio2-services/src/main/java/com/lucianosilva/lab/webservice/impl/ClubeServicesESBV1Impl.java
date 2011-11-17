9183c45472192790568e8b7d4272efc5bb0587ca
luciano@lucianosilva.com <luciano@lucianosilva.com@878331a1-c259-cb4f-48cd-a76829f63afa>
2011-11-17 02:25:12 +0000

    atualização da estrutura do projeto.

---
+++ trunk/blog/Laboratorio-2/laboratorio2-services/src/main/java/com/lucianosilva/lab/webservice/impl/ClubeServicesESBV1Impl.java
@@ -0,0 +1,40 @@
+/**
+ *  Licença Creative Commons.
+ *  Blog do Luciano Silva - www.lucianosilva.com
+ */
+package com.lucianosilva.lab.webservice.impl;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lucianosilva.lab.core.entity.Clube;
+import com.lucianosilva.lab.service.ClubeService;
+import com.lucianosilva.lab.webservice.ClubeServicesESBV1;
+
+/**
+ * 
+ *
+ * @autor luciano.silva
+ * @version $Rev$ $Date$
+ */
+@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+@WebService(endpointInterface="com.lucianosilva.lab.webservice.ClubeServicesESBV1")
+public class ClubeServicesESBV1Impl implements ClubeServicesESBV1 {
+
+	@Autowired
+	private ClubeService clubeService;
+	
+	/* (non-Javadoc)
+	 * @see com.lucianosilva.lab.webservice.ClubeServicesESBV1#listAll()
+	 */
+	public List<Clube> listAll() {
+		// 
+		return clubeService.listAll();
+	}
+
+}
